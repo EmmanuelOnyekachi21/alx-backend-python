5b34bb33fa91db343d784383666791dada0970f0
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2024-10-12 13:49:51 +0100

    Advanced Task 3

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -6,7 +6,17 @@
 
 from typing import List, Any
 
+
 def zoom_array(lst: List[Any], factor: int = 2) -> List[Any]:
+    """_summary_
+
+    Args:
+        lst (List[Any]): _description_
+        factor (int, optional): _description_. Defaults to 2.
+
+    Returns:
+        List[Any]: _description_
+    """
     zoomed_in: List[Any] = [
         item for item in lst
         for i in range(factor)
